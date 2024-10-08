11d048a82c092fddc979251ed23ee0207fd9172c
67sazzadhossen <sazzadhossen010@gmail.com>
2024-10-08 19:55:58 +0600

    aaa

--- src/Components/HomePageComponents/ServicesSection.jsx
+++ src/Components/HomePageComponents/ServicesSection.jsx
@@ -45,50 +45,50 @@ const ServicesSection = () => {
     },
   ];
 
-  const { scrollY } = useScroll();
-  const SECTION_HEIGHT = 4000;
+  //   const { scrollY } = useScroll();
+  //   const SECTION_HEIGHT = 4000;
 
-  const clip1 = useTransform(scrollY, [0, 4000], [0, 0]);
-  const clip2 = useTransform(scrollY, [0, 4000], [100, 100]);
+  //   const clip1 = useTransform(scrollY, [0, 4000], [0, 0]);
+  //   const clip2 = useTransform(scrollY, [0, 4000], [100, 100]);
 
-  const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}% )`;
+  //   const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}% )`;
 
-  const backgroundSize = useTransform(
-    scrollY,
-    [0, SECTION_HEIGHT + 500],
-    ["0%", "150%"]
-  );
-  const opacity = useTransform(
-    scrollY,
-    [SECTION_HEIGHT, SECTION_HEIGHT + 500],
-    [1, 0]
-  );
-  const rotate = useTransform(scrollY, [0, SECTION_HEIGHT + 500], [0, 360]);
+  //   const backgroundSize = useTransform(
+  //     scrollY,
+  //     [0, SECTION_HEIGHT + 500],
+  //     ["0%", "150%"]
+  //   );
+  //   const opacity = useTransform(
+  //     scrollY,
+  //     [SECTION_HEIGHT, SECTION_HEIGHT + 500],
+  //     [1, 0]
+  //   );
+  //   const rotate = useTransform(scrollY, [0, SECTION_HEIGHT + 500], [0, 360]);
 
-  const ParallaxImg = ({ className, alt, src, start, end }) => {
-    const ref = useRef(null);
+  //   const ParallaxImg = ({ className, alt, src, start, end }) => {
+  //     const ref = useRef(null);
 
-    const { scrollYProgress } = useScroll({
-      target: ref,
-      offset: [`${start}px end`, `end ${end * -1}px`],
-    });
+  //     const { scrollYProgress } = useScroll({
+  //       target: ref,
+  //       offset: [`${start}px end`, `end ${end * -1}px`],
+  //     });
 
-    const opacity = useTransform(scrollYProgress, [0.75, 1], [1, 0]);
-    const scale = useTransform(scrollYProgress, [0.75, 1], [1, 0.85]);
+  //     const opacity = useTransform(scrollYProgress, [0.75, 1], [1, 0]);
+  //     const scale = useTransform(scrollYProgress, [0.75, 1], [1, 0.85]);
 
-    const y = useTransform(scrollYProgress, [0, 1], [start, end]);
-    const transform = useMotionTemplate`translateY(${y}px) scale(${scale})`;
+  //     const y = useTransform(scrollYProgress, [0, 1], [start, end]);
+  //     const transform = useMotionTemplate`translateY(${y}px) scale(${scale})`;
 
-    return (
-      <motion.img
-        src={src}
-        alt={alt}
-        className={className}
-        ref={ref}
-        style={{ transform, opacity }}
-      />
-    );
-  };
+  //     return (
+  //       <motion.img
+  //         src={src}
+  //         alt={alt}
+  //         className={className}
+  //         ref={ref}
+  //         style={{ transform, opacity }}
+  //       />
+  //     );
+  //   };
   return (
     // <div className={"bg-black text-white sticky"}>
     //   <div
@@ -123,57 +123,59 @@ const ServicesSection = () => {
     //   </div>
     // </div>
 
-    <div className={"bg-black"}>
-      <div
-        style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}
-        className={"relative w-full"}
-      >
-        <motion.div
-          className="sticky top-0 h-screen w-full text-8xl text-white"
-          style={{
-            clipPath,
-            // rotate,
-            backgroundSize,
-            opacity,
-            backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/American_Broadcasting_Company_Logo.svg/2044px-American_Broadcasting_Company_Logo.svg.png)`,
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></motion.div>
-        {/* <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-zinc-950" /> */}
+    // <div className={"bg-black"}>
+    //   <div
+    //     style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}
+    //     className={"relative w-full"}
+    //   >
+    //     <motion.div
+    //       className="sticky top-0 h-screen w-full text-8xl text-white"
+    //       style={{
+    //         clipPath,
+    //         // rotate,
+    //         backgroundSize,
+    //         opacity,
+    //         backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/American_Broadcasting_Company_Logo.svg/2044px-American_Broadcasting_Company_Logo.svg.png)`,
+    //         backgroundPosition: "center",
+    //         backgroundRepeat: "no-repeat",
+    //       }}
+    //     ></motion.div>
+    //     {/* <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-zinc-950" /> */}
 
-        <div className="mx-auto max-w-5xl px-4 pt-[200px]">
-          <ParallaxImg
-            src="https://images.unsplash.com/photo-1484600899469-230e8d1d59c0?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="And example of a space launch"
-            start={200}
-            end={100}
-            className=""
-          />
-          <ParallaxImg
-            src="https://images.unsplash.com/photo-1446776709462-d6b525c57bd3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="An example of a space launch"
-            start={400}
-            end={200}
-            className=""
-          />
-          <ParallaxImg
-            src="https://images.unsplash.com/photo-1541185933-ef5d8ed016c2?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Orbiting satellite"
-            start={600}
-            end={200}
-            className=""
-          />
-          <ParallaxImg
-            src="https://images.unsplash.com/photo-1494022299300-899b96e49893?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Orbiting satellite"
-            start={800}
-            end={200}
-            className=""
-          />
-        </div>
-      </div>
-    </div>
+    //     <div className="mx-auto max-w-5xl px-4 pt-[200px]">
+    //       <ParallaxImg
+    //         src="https://images.unsplash.com/photo-1484600899469-230e8d1d59c0?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //         alt="And example of a space launch"
+    //         start={200}
+    //         end={100}
+    //         className=""
+    //       />
+    //       <ParallaxImg
+    //         src="https://images.unsplash.com/photo-1446776709462-d6b525c57bd3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //         alt="An example of a space launch"
+    //         start={400}
+    //         end={200}
+    //         className=""
+    //       />
+    //       <ParallaxImg
+    //         src="https://images.unsplash.com/photo-1541185933-ef5d8ed016c2?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //         alt="Orbiting satellite"
+    //         start={600}
+    //         end={200}
+    //         className=""
+    //       />
+    //       <ParallaxImg
+    //         src="https://images.unsplash.com/photo-1494022299300-899b96e49893?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //         alt="Orbiting satellite"
+    //         start={800}
+    //         end={200}
+    //         className=""
+    //       />
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div>services</div>
   );
 };
 

b75ad735d3eb52e6504796ebab6eb1297a30ec22
67sazzadhossen <sazzadhossen010@gmail.com>
2024-10-08 20:01:01 +0600

    take

---
+++ src/Components/HomePageComponents/OurServices.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const OurServices = () => {
+  return <div className={"text-white bg-black"}>Services</div>;
+};
+
+export default OurServices;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,14 +1,14 @@
 "use client";
 import HeroSection from "@/Components/HomePageComponents/HeroSection";
+import OurServices from "@/components/HomePageComponents/OurServices";
 import PortfolioSection from "@/Components/HomePageComponents/PortfolioSection";
-import ServicesSection from "@/components/HomePageComponents/ServicesSection";
 import MainLayout from "@/Layout/MainLayout";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <HeroSection></HeroSection>
-      <ServicesSection></ServicesSection>
+      <OurServices></OurServices>
       <PortfolioSection></PortfolioSection>
     </MainLayout>
   );

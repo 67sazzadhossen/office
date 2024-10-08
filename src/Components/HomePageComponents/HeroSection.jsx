a891a2774e4fa11f8ecc2f0143f5a2e148bce305
67sazzadhossen <sazzadhossen010@gmail.com>
2024-10-08 13:08:57 +0600

    adding responsive banner

--- src/Components/HomePageComponents/HeroSection.jsx
+++ src/Components/HomePageComponents/HeroSection.jsx
@@ -8,15 +8,15 @@ const HeroSection = () => {
   console.log(data?.data[0]);
   return (
     <div className="min-h-screen relative bg-black">
-      <div className=" h-screen w-[85%] mx-auto ">
+      <div className=" h-screen lg:max-w-[85%] mx-auto px-4 lg:px-0">
         <div className="flex h-screen  flex-col justify-center">
           {/* ======= description ===== */}
-          <h1 className="text-white text-8xl w-2/3 z-40  leading-tight font-bold tracking-wide">
+          <h1 className="text-white text-5xl lg:text-8xl w-3/4 z-40  leading-tight font-bold tracking-wide">
             {data?.data[0].title}
           </h1>
 
           {isAdmin && (
-            <div className={"absolute bottom-16"}>
+            <div className={"lg:absolute mt-6 lg:mt-0 bottom-16 z-10"}>
               <Modal
                 apiName="banner"
                 buttonName={"Update Banner"}
@@ -30,14 +30,14 @@ const HeroSection = () => {
             </div>
           )}
 
-          {/* ========= Image ======= */}
+          {/* ========= Image for large devices ======= */}
 
           <div
             style={{
               clipPath: "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)",
               backgroundImage: `url("${data?.data[0].image}}")`,
             }}
-            className="h-screen bg-cover bg-no-repeat w-3/4 absolute right-0 bottom-0"
+            className="h-screen bg-cover bg-no-repeat w-3/4 hidden lg:block absolute right-0 bottom-0 "
           >
             <div className="h-screen w-full bg-black opacity-60"></div>
           </div>
@@ -45,9 +45,19 @@ const HeroSection = () => {
             style={{
               clipPath: "polygon(90% 0, 100% 0, 75% 100%, 65% 100%)",
             }}
-            className="h-screen w-3/4 absolute bg-black z-10 -left-80"
+            className="h-screen w-3/4 hidden lg:block absolute bg-black z-10 -left-80"
           ></div>
         </div>
+      </div>
+
+      {/* ========= imgae for small devices ========= */}
+      <div
+        style={{ backgroundImage: `url("${data?.data[0].image}}")` }}
+        className={
+          "h-screen absolute w-full bg-red-400 top-0 bg-no-repeat bg-cover z-0"
+        }
+      >
+        <div className="h-screen w-full bg-black opacity-50"></div>
       </div>
     </div>
   );

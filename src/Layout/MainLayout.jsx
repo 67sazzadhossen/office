611dac7277fcb0f14615cd036aa71d7def803f30
67sazzadhossen <sazzadhossen010@gmail.com>
2024-10-08 21:46:37 +0600

    lenis

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,25 +1,36 @@
 "use client";
 import Navbar from "@/Components/Shared/Navbar/Navbar";
 import useLoadData from "@/Hooks/useLoadData";
+import ReactLenis from "lenis/dist/lenis-react";
 
 const MainLayout = ({ children }) => {
   const [, , nav = isLoading] = useLoadData("navbar");
   const [, , banner = isLoading] = useLoadData("banner");
 
   return (
-    <div>
-      {nav || banner ? (
-        <div className="min-h-screen flex justify-center items-center bg-black text-white">
-          <span className="loading loading-infinity w-36"></span>
-        </div>
-      ) : (
-        <div>
-          <Navbar></Navbar>
-          <div className="">{children}</div>
-          <footer>Footer</footer>
-        </div>
-      )}
-    </div>
+    <ReactLenis
+      root
+      options={{
+        // Learn more -> https://github.com/darkroomengineering/lenis?tab=readme-ov-file#instance-settings
+        lerp: 0.05,
+        infinite: true,
+        syncTouch: true,
+      }}
+    >
+      <div>
+        {nav || banner ? (
+          <div className="min-h-screen flex justify-center items-center bg-black text-white">
+            <span className="loading loading-infinity w-36"></span>
+          </div>
+        ) : (
+          <div>
+            <Navbar></Navbar>
+            <div className="">{children}</div>
+            <footer>Footer</footer>
+          </div>
+        )}
+      </div>
+    </ReactLenis>
   );
 };
 

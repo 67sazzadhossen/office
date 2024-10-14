8aae36ee7253d35e2507f34912d41ecda5a0256a
67sazzadhossen <sazzadhossen010@gmail.com>
2024-10-14 10:22:25 +0600

    plans

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@
 import HeroSection from "@/Components/HomePageComponents/HeroSection";
 import OurServices from "@/Components/HomePageComponents/OurServices";
 import PortfolioSection from "@/Components/HomePageComponents/PortfolioSection";
+import PricingSection from "@/Components/HomePageComponents/PricingSection";
 import MainLayout from "@/Layout/MainLayout";
 
 const HomePage = () => {
@@ -10,6 +11,7 @@ const HomePage = () => {
       <HeroSection></HeroSection>
       <OurServices></OurServices>
       <PortfolioSection></PortfolioSection>
+      <PricingSection></PricingSection>
     </MainLayout>
   );
 };
